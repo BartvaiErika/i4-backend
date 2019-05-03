0c943b3765aa0d29181f276ae27fdec7c2c7828f
BartvaiErika <erika.bartvai@gmail.com>
2019-05-03 13:53:16 +0200

    week11 update

--- Week11/lechefusingsecurity/src/main/java/com/example/lechef/configuration/RestTemplateConfiguration.java
+++ Week11/lechefusingsecurity/src/main/java/com/example/lechef/configuration/RestTemplateConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.lechef.configuration;
 
+import com.example.lechef.persistence.Recipe;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,5 +11,9 @@ public class RestTemplateConfiguration {
   @Bean
   RestTemplate restTemplate() {
     return new RestTemplate();
+  }
+
+  public Recipe postForObject(String url, Recipe recipe) {
+    return Recipe.builder().build();
   }
 }
--- Week11/lechefusingsecurity/src/main/java/com/example/lechef/logic/KitchenManager.java
+++ Week11/lechefusingsecurity/src/main/java/com/example/lechef/logic/KitchenManager.java
@@ -1,4 +1,23 @@
 package com.example.lechef.logic;
 
+import com.example.lechef.configuration.RestTemplateConfiguration;
+import com.example.lechef.persistence.Recipe;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class KitchenManager {
+
+  private RestTemplateConfiguration restTemplate;
+
+  @Value("${recipes.url}")
+  private String url;
+
+  public Recipe saveRecipe(Recipe newRecipe) {
+    return restTemplate.postForObject(url, newRecipe);
+  }
 }
